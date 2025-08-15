99a597935477e3d8f05f8e87e78eae6e1c527ed8
atikhonov <tikhonovaf@gmail.com>
2025-08-15 19:40:12 +0300

    Доработана структура БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,51 @@
-INSERT INTO client(
-    id, name)
-VALUES (1, 'org1');
+INSERT INTO v_client( n_client_id, vc_code, vc_name)
+VALUES
+    (1, 'orgCode1', 'orgName1' ),
+    (2, 'orgCode2', 'orgName2'),
+    (3, 'orgCode3', 'orgName3'),
+    (4, 'orgCode4', 'orgName4'),
+    (5, 'orgCode5', 'orgName5'),
+    (6, 'orgCode6', 'orgName6'),
+    (7, 'orgCode7', 'orgName7'),
+    (8, 'orgCode8', 'orgName8')
+;
 
+INSERT INTO v_store( n_store_id, vc_code, vc_name)
+VALUES
+    (1, 'storeCode1', 'storeName1' ),
+    (2, 'storeCode2', 'storeName2'),
+    (3, 'storeCode3', 'storeName3'),
+    (4, 'storeCode4', 'storeName4'),
+    (5, 'storeCode5', 'storeName5'),
+    (6, 'storeCode6', 'storeName6'),
+    (7, 'storeCode7', 'storeName7'),
+    (8, 'storeCode8', 'storeName8')
+;
 
-INSERT INTO client_user(
-    id, first_name, last_name, client_id, login, password)
+INSERT INTO client_user(n_user_id, vc_first_name, vc_last_name, n_client_id, vc_login, vc_password)
 VALUES (1, 'ADMIN', 'ADMIN', 1,  'ADMIN', '8257a3811b9f6bb9d59dfb3931e220fa5574cee38fff551066caca1a50b1691ebdffa87f2d7213910e8bdbcf4d669c2756e57196667dd8f5e8af66971b2');
+
+INSERT INTO loading_point( n_loading_point_id, n_store_id, vc_code, vc_name, vc_comment)
+VALUES
+    (1, 1, 'loading_point_Code1', 'loading_point_Name1' , 'loading_point_Comment_1' ),
+    (2, 1, 'loading_point_Code2', 'loading_point_Name2' , 'loading_point_Comment_2' ),
+    (3, 1, 'loading_point_Code3', 'loading_point_Name3' , 'loading_point_Comment_3' ),
+    (4, 2, 'loading_point_Code1', 'loading_point_Name1' , 'loading_point_Comment_1' ),
+    (5, 2, 'loading_point_Code2', 'loading_point_Name3' , 'loading_point_Comment_2' ),
+    (6, 3, 'loading_point_Code1', 'loading_point_Name1' , 'loading_point_Comment_1' ),
+    (7, 3, 'loading_point_Code2', 'loading_point_Name3' , 'loading_point_Comment_2' ),
+    (8, 3, 'loading_point_Code1', 'loading_point_Name1' , 'loading_point_Comment_1' )
+;
+
+
+    INSERT INTO slot( n_slot_id, d_date, n_loading_point_id, n_client_id, d_start_time, d_end_time, vc_status)
+    VALUES
+(1, '2025-08-16', 1, 1, '14:30:00', '15:30:00' , 'B' ),
+(2, '2025-08-16', 1, 2, '15:30:00', '16:30:00' , 'B' ),
+(3, '2025-08-16', 1, 3, '15:30:00', '17:30:00' , 'B' ),
+(4, '2025-08-16', 3, 5, '14:30:00', '15:30:00' , 'B' ),
+(5, '2025-08-16', 3, 6, '15:30:00', '16:30:00' , 'B' ),
+(6, '2025-08-16', 5, 4, '10:30:00', '11:30:00' , 'B' ),
+(7, '2025-08-16', 5, 7, '11:30:00', '12:30:00' , 'B' )
+    ;
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,59 @@
--- Table: public.client_user
+-- Список покупателей (из view)
 
-CREATE TABLE IF NOT EXISTS client
+CREATE TABLE IF NOT EXISTS v_client
 (
-    id bigint NOT NULL,
-    name varchar(100),
-    CONSTRAINT client_pkey PRIMARY KEY (id)
+    n_client_id bigint NOT NULL,
+    vc_code varchar(100),
+    vc_name varchar(1000)
     )
 ;
 
--- Table: public.client_user
+
+CREATE TABLE IF NOT EXISTS v_store
+(
+    n_store_id bigint NOT NULL,
+    vc_code varchar(100),
+    vc_name varchar(1000)
+    )
+;
+
+--  Список пользователей (новая таблица)
+--  Table: client_user
 
 CREATE TABLE IF NOT EXISTS client_user (
-   id bigint NOT NULL,
-   first_name varchar(100),
-    last_name varchar(100),
-    second_name varchar(100),
-    email varchar(100),
-    start_date date,
-    end_date date,
-    client_id bigint NOT NULL,
-    phone varchar(100),
-    login varchar(100),
-    password varchar(1000),
-    CONSTRAINT client_user_pkey PRIMARY KEY (id)
+    n_user_id bigint NOT NULL,
+    vc_first_name varchar(100),
+    vc_last_name varchar(100),
+    vc_second_name varchar(100),
+    vc_email varchar(100),
+    d_start_date date,
+    d_end_date date,
+    n_client_id bigint NOT NULL,
+    vc_phone varchar(100),
+    vc_login varchar(100),
+    vc_password varchar(1000),
+    CONSTRAINT client_user_pkey PRIMARY KEY (n_user_id)
+    );
+
+-- Список пунктов налива (новая таблица)
+CREATE TABLE IF NOT EXISTS loading_point (
+    n_loading_point_id bigint NOT NULL,
+    n_store_id bigint NOT NULL,
+    vc_code varchar(100),
+    vc_name varchar(100),
+    vc_comment varchar(400),
+    CONSTRAINT loading_point_pkey PRIMARY KEY (n_loading_point_id)
+    );
+
+-- Список слотов (новая таблица)
+CREATE TABLE IF NOT EXISTS slot (
+     n_slot_id bigint NOT NULL,
+     d_date date,
+     n_loading_point_id bigint NOT NULL,
+     n_client_id bigint NOT NULL,
+     d_start_time time,
+     d_end_time time,
+     vc_status varchar(1),
+    CONSTRAINT slot_pkey PRIMARY KEY (n_slot_id)
     );
 

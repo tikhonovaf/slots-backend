f8d6e5084cc81495cda7bf8907049effe09e25f3
atikhonov <tikhonovaf@gmail.com>
2025-10-24 22:39:21 +0300

    Доработаны pom

--- src/main/java/ru/ttk/slotsbe/backend/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/ru/ttk/slotsbe/backend/exceptionhandler/GlobalExceptionHandler.java
@@ -1,14 +1,33 @@
 package ru.ttk.slotsbe.backend.exceptionhandler;
-
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.ttk.slotsbe.backend.exception.ValidateException;
 
+
 @ControllerAdvice
+@OpenAPIDefinition(info = @Info(title = "API", version = "v1"))
+@Hidden // из io.swagger.v3.oas.annotations.Hidden
 public class GlobalExceptionHandler {
-    @ExceptionHandler
-    public ResponseEntity<AppError> catchResourceNotFoundException(ValidateException e) {
-        return new ResponseEntity<>(new AppError(e.getStatus().value(), e.getMessage()), e.getStatus());
+
+    @ExceptionHandler(ValidateException.class)
+    public ResponseEntity<AppError> handleValidateException(ValidateException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(new AppError(e.getStatus().value(), e.getMessage()));
     }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<AppError> handleConstraintViolation(ConstraintViolationException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new AppError(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+    }
+
+    // Можно добавить другие ExceptionHandler'ы по необходимости
 }
